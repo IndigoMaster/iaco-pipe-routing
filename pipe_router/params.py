dd3204165140dd8ddf7a7116aca74d597de67f75
IndigoMaster <nhill.3ds@gmail.com>
2022-12-23 21:34:35 -0600

    initial commit

---
+++ pipe_router/params.py
@@ -0,0 +1,84 @@
+"""
+Params object definition.
+"""
+
+from typing import List, Optional
+
+from pipe_router.bounding_box import BoundingBox
+from pipe_router.grid import Grid
+from pipe_router.pipe_route import PipeRoute
+from pipe_router.solvers.solverbase import SolverBase
+
+
+class Params:
+    """
+    Data class for storing parameters read from a config file.
+    """
+
+    def __init__(self):
+        """
+        Constructs an empty Params object
+        """
+        self.scenario_title: str = "No Title"
+        self.volume: Optional[BoundingBox] = None
+        self.grid: Optional[Grid] = None
+        self.occupied_space_boxes: List[BoundingBox] = []
+        self.free_space_boxes: List[BoundingBox] = []
+        self.pipe_routes: List[PipeRoute] = []
+        self.solver_list: List[SolverBase] = []
+
+    def set_volume(self, volume: BoundingBox) -> None:
+        """
+        Sets the volume for these params.
+
+        :param volume: volume bounding box
+        """
+        self.volume = volume
+
+    def set_grid(self, grid: Grid) -> None:
+        """
+        Sets the grid object for these params.
+
+        :param grid: grid object
+        """
+        self.grid = grid
+
+    def add_occupied_space_box(self, bb: BoundingBox) -> None:
+        """
+        Appends a new occupied space bounding box to the collection.
+
+        :param bb: bounding box representing an occupied space
+        """
+        self.occupied_space_boxes.append(bb)
+
+    def add_free_space_box(self, bb: BoundingBox) -> None:
+        """
+        Appends a new free space bounding box to the collection.
+
+        :param bb: bounding box representing a free space
+        """
+        self.free_space_boxes.append(bb)
+
+    def add_pipe_route(self, route: PipeRoute) -> None:
+        """
+        Appends a new pipe route object to the collection.
+
+        :param route: pipe route object
+        """
+        self.pipe_routes.append(route)
+
+    def add_solver(self, solver: SolverBase) -> None:
+        """
+        Appends a new solver object to the collection.
+
+        :param solver: solver obj
+        """
+        self.solver_list.append(solver)
+
+    def set_scenario_title(self, title: str) -> None:
+        """
+        Sets the current title.
+
+        :param title: title string
+        """
+        self.scenario_title = title
