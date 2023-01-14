227a0c1178431f5699a55d3337d2a44bb05273b6
IndigoMaster <61066648+IndigoMaster@users.noreply.github.com>
2023-01-14 12:36:36 -0600

    rework graph logic with adjacency lists

--- pipe_router/graph.py
+++ pipe_router/graph.py
@@ -33,10 +33,27 @@ class UndirectedGraph:
         :param key2: key for second node
         :return: stored value
         """
-        if key1 not in self.graph or key2 not in self.graph:
-            missing_keys = [key for key in [key1, key2] if key not in self.graph]
-            raise KeyError(f'The following keys are missing: {missing_keys}')
-        return self.graph[key1][key2]
+        try:
+            return self.edges[key1][key2]
+        except KeyError:
+            pass
+
+        if key1 not in self.edges or key2 not in self.edges:
+            raise KeyError(f'The following adjacency keys are missing: {[key for key in [key1, key2] if key not in self.edges]}')
+        if key2 not in self.edges[key1]:
+            raise KeyError(f'Node key "{key2}" is not connected to node key "{key1}"')
+
+    def get_connections_to(self, key: any) -> List[any]:
+        """
+        Gets all nodes (keys) connected to this node (key).
+
+        :param key: key to inspect
+        :return: list of keys, possibly empty
+        """
+        if key not in self.edges:
+            return []
+        else:
+            return list(self.edges[key].keys())
 
     def set_node_val(self, key: any, value: any) -> None:
         """
@@ -57,11 +74,17 @@ class UndirectedGraph:
         :param value: value to store
         :return: None
         """
-        if key1 not in self.graph and key2 not in self.graph:
-            self.graph[key1] = {key2: value}
-            self.graph[key2] = self.graph[key1]
+        if key1 not in self.edges.keys():
+            self.edges[key1] = {key2: value}
         else:
-            self.graph[key1][key2] = value
+            # self.edges[key1] |= {key2: value}
+            self.edges[key1][key2] = value
+
+        if key2 not in self.edges.keys():
+            self.edges[key2] = {key1: value}
+        else:
+            # self.edges[key2] |= {key1: value}
+            self.edges[key2][key1] = value
 
     def set_all_edges(self, val: any) -> None:
         """
@@ -70,9 +93,20 @@ class UndirectedGraph:
         :param val: value to store in every edge
         :return: None
         """
-        for key1 in self.graph:
-            for key2 in self.graph[key1]:
-                self.graph[key1][key2] = val
+        for key1 in self.edges.keys():
+            for key2 in self.edges[key1].keys():
+                self.edges[key1][key2] = val
+
+    def update_edges(self, func: Callable[[any], any]) -> None:
+        """
+        Updates every graph edge using the provided function object.
+
+        :param func: must accept graph edge value input and return an updated value
+        """
+        for key1 in self.edges:
+            for key2 in self.edges[key1]:
+                val = func(self.get_edge(key1, key2))
+                self.set_edge(key1, key2, val)
 
     def __str__(self):
         return self.__class__.__name__ + '\n' + str(self.edges)
